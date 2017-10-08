7efc1881efc34d5731b3fecfb25be533d42a86cf
tue88219 <31864160+tue88219@users.noreply.github.com>
2017-10-07 22:56:25 -0400

    Created Initial Project Skeleton

--- src/edu/temple/cis/c3238/banksim/BankSimMain.java
+++ src/edu/temple/cis/c3238/banksim/BankSimMain.java
@@ -1,5 +1,9 @@
 package edu.temple.cis.c3238.banksim;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @author Cay Horstmann
  * @author Modified by Paul Wolfgang
@@ -9,9 +13,11 @@ public class BankSimMain {
     public static final int NACCOUNTS = 10;
     public static final int INITIAL_BALANCE = 10000;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        
         Bank b = new Bank(NACCOUNTS, INITIAL_BALANCE);
         Thread[] threads = new Thread[NACCOUNTS];
+        Thread testThread = new TestingThread(b);
         // Start a thread for each account
         for (int i = 0; i < NACCOUNTS; i++) {
             threads[i] = new TransferThread(b, i, INITIAL_BALANCE);
@@ -25,8 +31,11 @@ public class BankSimMain {
                 // Ignore this
             }
         }
-        b.test();
+        testThread.start();
     }
+
 }
+
+
 
 
